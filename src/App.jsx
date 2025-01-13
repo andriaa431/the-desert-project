d2cdcb9498689dbdce19c649b3d2e8999cf91cb0
andriaa431 <andriagiuna2011@email.com>
2025-01-13 20:01:11 +0400

    added add to cart and added the usestate for add to cart button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-import { AddToCart , RemoveItem } from './Icons'
+import { AddToCart , RemoveItem, CarbonNeutral, IncrementQuantity, DecrementQuantity, EmptyCart, OrderConfirmed } from './Icons'
 import dessertproducts from './data.json'
+import { useState } from 'react'
 
 
+ 
 const base_url = "https://res.cloudinary.com/dc2c49xov/desserts/"
 
 function App() {
+const [numberoforders, setnumberoforders] = useState(0);
  
-
+const Addorder = () => {
+  setnumberoforders(numberoforders + 1);
+}
 
   return (
     
@@ -17,17 +22,22 @@ function App() {
    {dessertproducts.map((dessert) => {
       return <>
       <div className="dessert"><img  src={base_url + dessert.images.desktop} />
-      <button id='add-button'><AddToCart /> Add To Cart</button>
+      <button onClick={() => Addorder()} id='add-button'><AddToCart /> Add To Cart</button>
       <p id='title'>{dessert.category}</p>
       <p id='name'>{dessert.name}</p>
       <p id='price'>{`$${dessert.price}`}</p>
       </div>
-      <br></br>
     
       
     </>  
    })}</div>
-   <RemoveItem />
+   <div className="cart-list">
+  
+        <h3>Youre Cart ({numberoforders})</h3>
+        <EmptyCart />  
+        <p>Youre added items will appear here</p>
+   </div>
+
   
    </div>
   )
